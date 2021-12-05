9eb8645f169ae0de182525fa0abfa9ed99ddb465
g.saintetienne <g.saintetienne@primobox.com>
2021-12-05 19:00:10 +0100

    first seed

---
+++ HydrothermalVentureTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace Namespace2Test
+{
+    public class HydrothermalVentureTests
+    {
+        [Fact]
+        public void Expected_minutes_in_oven()
+        {
+            Assert.Equal(40, 40);
+        }
+
+
+    }
+}
